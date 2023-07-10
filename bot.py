08143865a80a118a54d801f1e3f91a9ba0d880ac
Hannes Albert <hannes10042004@icloud.com>
2023-07-10 22:59:15 +0200

    Added support for multiple languages

--- bot.py
+++ bot.py
@@ -4,8 +4,6 @@ from discord import app_commands
 from itertools import cycle
 
 
-
-
 # Send messages
 async def send_message(message, user_message):
     try:
@@ -14,6 +12,10 @@ async def send_message(message, user_message):
 
     except Exception as e:
         print(e)
+
+
+async def setlang(l):
+    responses.language(l)
 
 
 def run_discord_bot():
@@ -52,5 +54,59 @@ def run_discord_bot():
     @tree.command(name="turn", description="Tells you, whose turn it is", guild=discord.Object(id=guild_id))
     async def turn_command(interaction):
         await interaction.response.send_message(f'It is {responses.turn()}\'s turn')
+
+    @tree.command(name="langto_german", description="used to set game language to german",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_german(interaction):
+        await setlang("de")
+        await interaction.response.send_message("The language has been set to german")
+
+    @tree.command(name="langto_english", description="used to set game language to english",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("en")
+        await interaction.response.send_message("The language has been set to english")
+
+    @tree.command(name="langto_bengali", description="used to set game language to bengali",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("bn")
+        await interaction.response.send_message("The language has been set to bengali")
+
+    @tree.command(name="langto_spanish", description="used to set game language to spanish",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("es")
+        await interaction.response.send_message("The language has been set to spanish")
+
+    @tree.command(name="langto_french", description="used to set game language to french",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("fr")
+        await interaction.response.send_message("The language has been set to french")
+
+    @tree.command(name="langto_pl", description="used to set game language to polish",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("pl")
+        await interaction.response.send_message("The language has been set to polish")
+
+    @tree.command(name="langto_italian", description="used to set game language to italian",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("it")
+        await interaction.response.send_message("The language has been set to italian")
+
+    @tree.command(name="langto_romanian", description="used to set game language to romanian",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("ro")
+        await interaction.response.send_message("The language has been set to romanian")
+
+    @tree.command(name="langto_portuguese", description="used to set game language to portuguese",
+                  guild=discord.Object(id=guild_id))
+    async def set_lang_english(interaction):
+        await setlang("pt")
+        await interaction.response.send_message("The language has been set to portuguese")
 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,11 +1,12 @@
 import sys
 from itertools import cycle
 
-from PyDictionary import PyDictionary
+from PyMultiDictionary import MultiDictionary
 
+dictionary = MultiDictionary()
 p_array = [sys.maxsize]
-dictionary = PyDictionary()
 
+lang = 'de'
 
 # list to handle turns
 players = ["player0", "player1"]
@@ -13,7 +14,7 @@ myIterator = cycle(range(2))
 
 
 def turn() -> str:
-    players[myIterator.__next__()]
+    var = players[myIterator.__next__()]
     return players[myIterator.__next__()]
 
 
@@ -24,7 +25,8 @@ def handle_response(massage) -> str:
         if p_massage == 'obi':
             return 'Hello there! \n'
         elif p_massage[0] == 'p':
-            if dictionary.meaning(massage) is None:
+            global lang
+            if str(dictionary.meaning(lang, massage))[2] == ']':
                 return f'{p_massage} is not a word. Now it is {players[myIterator.__next__()]}\'s turn'
             elif p_massage in p_array:
                 return f'{p_massage} was mentioned already. Now it is {players[myIterator.__next__()]}\'s turn'
@@ -32,3 +34,10 @@ def handle_response(massage) -> str:
                 p_array.append(p_massage)
                 return f'Good Job, {p_massage} is a new word. Now it is {players[myIterator.__next__()]}\'s turn'
     return ""
+
+
+def language(l):
+    global p_array
+    p_array.clear()
+    global lang
+    lang = l
